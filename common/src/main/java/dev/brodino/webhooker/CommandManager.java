274363051e5dd5cf95160955701e7f7c69214e7c
Brodino <brodino96@gmail.com>
2026-03-07 12:11:18 +0100

    feat(commands): implemented direct command to send webhook

--- common/src/main/java/dev/brodino/webhooker/CommandManager.java
+++ common/src/main/java/dev/brodino/webhooker/CommandManager.java
@@ -1,18 +1,41 @@
 package dev.brodino.webhooker;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.Component;
 
-public class Commands {
+import java.util.Set;
+
 public class CommandManager {
+
+    private static final SuggestionProvider<CommandSourceStack> AVAILABLE_CHANNELS = ((context, builder) -> {
+        Set<String> configuredChannels = Webhooker.CONFIG.getChannelList().keySet();
+        for (String channel : configuredChannels) {
+            builder.suggest(channel);
+        }
+
+        return builder.buildFuture();
+    });
+
+    private static final SuggestionProvider<CommandSourceStack> AVAILABLE_TAGS = ((context, builder) -> {
+        Set<String> configuredChannels = Webhooker.CONFIG.getTaggableList().keySet();
+        for (String channel : configuredChannels) {
+            builder.suggest(channel);
+        }
+
+        return builder.buildFuture();
+    });
 
     public static void initialize(CommandDispatcher<CommandSourceStack> dispatcher) {
         dispatcher.register(Commands.literal(Webhooker.MOD_ID)
             .requires(src -> src.hasPermission(2))
             .then(getReloadCommand())
+            .then(getSendCommand())
         );
     }
 
@@ -29,6 +52,41 @@ public class CommandManager {
                 Webhooker.LOGGER.error("Config reloaded");
                 context.getSource().sendSystemMessage(Component.literal("Config reloaded"));
                 return 1;
-            });
+            }
+        );
+    }
+
+    private static LiteralArgumentBuilder<CommandSourceStack> getSendCommand() {
+        return Commands.literal("send")
+            .then(Commands.argument("sender", StringArgumentType.word())
+                .then(Commands.argument("message", StringArgumentType.string())
+                    .then(Commands.argument("channel", StringArgumentType.word())
+                        .suggests(AVAILABLE_CHANNELS)
+                        .executes((CommandContext<CommandSourceStack> context) -> {
+                            Discord.sendMessage(
+                                StringArgumentType.getString(context, "sender"),
+                                StringArgumentType.getString(context, "message"),
+                                new String[]{StringArgumentType.getString(context, "channel")},
+                                null
+                            );
+                            return 1;
+                        })
+
+                        .then(Commands.argument("tags", StringArgumentType.greedyString())
+                            .suggests(AVAILABLE_TAGS)
+                            .executes((CommandContext<CommandSourceStack> context) -> {
+                                Discord.sendMessage(
+                                    StringArgumentType.getString(context, "sender"),
+                                    StringArgumentType.getString(context, "message"),
+                                    new String[]{StringArgumentType.getString(context, "channel")},
+                                    StringArgumentType.getString(context, "tags").split(" ")
+                                );
+                                return 1;
+                            }
+                        )
+                    )
+                )
+            )
+        );
     }
 }
